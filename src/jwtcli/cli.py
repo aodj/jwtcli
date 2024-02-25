d843ba58def697eb9b74725603885447c14c4823
Alexander O'Donovan-Jones <alexodonovanjones@gmail.com>
2024-02-25 18:23:58 +0000

    Remove unused function

--- src/jwtcli/cli.py
+++ src/jwtcli/cli.py
@@ -1,5 +1,4 @@
 import json
-from typing import TextIO
 
 import click
 from click_default_group import DefaultGroup
@@ -34,24 +33,3 @@ def encode(payload: str, algorithm: str, secret: str):
             algorithm=algorithm,
         )
     )
-
-
-@cli.command()
-@click.option(
-    "-pk",
-    "--private-key",
-    type=click.STRING,
-    required=False,
-)
-@click.option(
-    "-pkf",
-    "--private-key-file",
-    type=click.File(),
-    required=False,
-)
-def get(private_key: str | None, private_key_file: TextIO | None):
-    click.echo(repr(".venv/\n\nrequirements.txt\nrequirements-dev.txt\n"))
-    if private_key:
-        click.echo(repr(private_key))
-    if private_key_file:
-        click.echo(repr(private_key_file.read()))
