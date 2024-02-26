7221143068dbf3117f14b499ce14dcc51abb1c7c
Alexander O'Donovan-Jones <alexodonovanjones@gmail.com>
2024-02-26 23:08:09 +0000

    Add --private-key and --private-key-file

--- src/jwtcli/cli.py
+++ src/jwtcli/cli.py
@@ -1,5 +1,7 @@
 import json
 import sys
+from cryptography.hazmat.primitives.serialization import load_pem_private_key
+from typing import TextIO
 
 import click
 from click_default_group import DefaultGroup
@@ -34,9 +36,38 @@ def check_for_pipe(ctx, param, value):
     default="HS256",
     show_default=True,
 )
-@click.option("-s", "--secret", type=click.STRING, default="", required=False)
 @click.option("-h", "--as-header", is_flag=True, default=False, required=False)
-def encode(payload: str, algorithm: str, secret: str, as_header: bool):
+@click.option("-s", "--secret", type=click.STRING, default="", required=False)
+@click.option("-pk", "--private-key", type=click.STRING, default="", required=False)
+@click.option("-pkf", "--private-key-file", type=click.File(), required=False)
+@click.option("-p", "--password", type=click.STRING, default=None, required=False)
+def encode(
+    payload: str,
+    algorithm: str,
+    secret: str,
+    as_header: bool,
+    private_key: str,
+    private_key_file: TextIO,
+    password: str | None,
+):
+    if algorithm in ("RS256"):
+        # need to ensure we have the necessary arguments
+        assert private_key or private_key_file
+        # We need to convert the input to the correct format:
+        # a) It needs to be bytes and not a string, hence the `.encode()`
+        # b) We need to ensure line breaks are correctly handled.
+        #    The input given is 'foo\nbar' which when encoded becomes
+        #    b'foo\\nbar' so the replace is to correct for that.
+        # breakpoint()
+        if private_key:
+            secret = load_pem_private_key(
+                private_key.encode().replace(b"\\n", b"\n"), password=password
+            )
+        elif private_key_file:
+            secret = load_pem_private_key(
+                private_key_file.read().encode(), password=password
+            )
+
     token = jwt.encode(
         payload=json.loads(payload),
         key=secret,
